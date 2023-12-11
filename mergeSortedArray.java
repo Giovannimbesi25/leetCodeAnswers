7dbc15329090cf5e68b7fcbfaec57b4b0a49672e
Giovanni Imbesi <giovanni@pop-os-giovanni.station>
2023-12-11 18:40:35 +0100

    mergeSortedArray

---
+++ mergeSortedArray.java
@@ -0,0 +1,47 @@
+/*
+    You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, 
+    and two integers m and n, representing the number of elements in nums1 and nums2 respectively.
+    Merge nums1 and nums2 into a single array sorted in non-decreasing order.
+    The final sorted array should not be returned by the function, but instead be stored inside the array nums1. 
+    To accommodate this, nums1 has a length of m + n, where the first m elements denote the elements that should be merged, 
+    and the last n elements are set to 0 and should be ignored. nums2 has a length of n.
+
+
+    Complessità della soluzione proposta O(m+n)
+*/
+
+public class mergeSortedArray {
+
+    public static void merge(int[] nums1, int m, int[] nums2, int n) {
+        int i = m - 1;
+        int j = n - 1;
+        int k = m + n - 1;
+
+        while (j >= 0) {
+            if (i >= 0 && nums1[i] > nums2[j]) {
+                nums1[k] = nums1[i];
+                i--;
+            } else {
+                nums1[k] = nums2[j];
+                j--;
+            }
+            k--;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] nums1 = new int[6]; // Assuming nums1 has enough space for merging
+        nums1[0] = 1;
+        nums1[1] = 3;
+        nums1[2] = 5;
+        int[] nums2 = {2, 4, 6};
+        int m = 3;
+        int n = 3;
+
+        merge(nums1, m, nums2, n);
+
+        for (int num : nums1) {
+            System.out.print(num + " ");
+        }
+    }
+}
